2917b6c44be047ebf2aa6ede903057e29bee5470
colorleague <colorleague.com@gmail.com>
2016-07-18 00:02:24 -0400

    [NJ] Express & Karma/Jasmine
    Just a quick save.

--- MEAN/src/config.js
+++ MEAN/src/config.js
@@ -12,11 +12,123 @@ app.directive("logo", function(){
   }
 });
 
+app.directive("light", function(){
+	return {
+    restrict: 'E',
+    template: require('directives/light.html')
+	}
+})
+
+app.directive("desk1", function(){
+	return {
+    restrict: 'E',
+    template: require('directives/desk1.html')
+	}
+})
+
+app.directive("desk2", function(){
+	return {
+    restrict: 'E',
+    template: require('directives/desk2.html')
+	}
+})
+
+app.directive("desk3", function(){
+	return {
+    restrict: 'E',
+    template: require('directives/desk3.html')
+	}
+})
+
+app.directive("desk4", function(){
+	return {
+    restrict: 'E',
+    template: require('directives/desk4.html')
+	}
+})
+
+app.directive("man", function(){
+	return {
+    restrict: 'E',
+    template: require('directives/man.html')
+	}
+})
+
+app.directive("face1", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/face1.html')
+  }
+})
+
+app.directive("face2", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/face2.html')
+  }
+})
+
+app.directive("face3", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/face3.html')
+  }
+})
+
+app.directive("face4", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/face4.html')
+  }
+})
+
+app.directive("face5", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/face5.html')
+  }
+})
+
+
 app.directive("menu", function(){
   return {
     restrict: 'E',
-    controller: menuController,
     template: require('directives/menu.html')
+  }
+});
+
+app.directive("menu2", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/menu2.html')
+  }
+});
+
+app.directive("menu3", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/menu3.html')
+  }
+});
+
+app.directive("bubble1", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/bubble1.html')
+  }
+});
+
+app.directive("bubble2", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/bubble2.html')
+  }
+});
+
+app.directive("bubble3", function(){
+  return {
+    restrict: 'E',
+    template: require('directives/bubble3.html')
   }
 });
 
@@ -27,6 +139,12 @@ app.config(($stateProvider, $urlRouterProvider, $locationProvider) => {
 	$urlRouterProvider.otherwise('/');
 
 	$stateProvider
+	.state('animation', {
+		url: '/',
+		controller: menuController,
+		template: require('animation/animation.html')
+	})
+
 	.state('todos', {
 		url: '/',
 		template: require('todos/todos.html')
--- MEAN/src/controller/menu.js
+++ MEAN/src/controller/menu.js
@@ -1,9 +1,17 @@
 export default function($scope, $http, $state ) {
 
 	$http.get('http://api.colorleague.com/wp-json/posts')
-		.success(function(response){
-			$scope.wordpress = response;
-			console.log(response)
-		});
+	.success(function(response){
+		$scope.wordpress = response;
+		console.log(response)
+	});
+
+	$scope.pagination = {
+		count: 3
+	};
+
+	$scope.paginationToggle = function (){
+		$scope.pagination.count = $scope.pagination.count + 4
+	};
 
 }
